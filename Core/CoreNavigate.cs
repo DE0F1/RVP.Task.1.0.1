e04a2b1900efff5669eb5515de3ae50bda06222e
deofi <deofi@DESKTOP-E5PHOB4>
2023-02-14 08:03:29 +0300

    Добавьте файлы проекта.

---
+++ Core/CoreNavigate.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows.Controls;
+
+namespace PRV.Task._1.Core
+{
+    public static class CoreNavigate
+    {
+        public static Frame? MyConnection { get; set; }
+    }
+}
---
+++ View/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using PRV.Task._1.Core;
+using PRV.Task._1.View.Pages;
+using System.Windows;
+
+namespace PRV.Task._1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            CoreNavigate.MyConnection = MainFrame;
+            MainFrame.Navigate(new MainPage());
+
+        }
+    }
+}
